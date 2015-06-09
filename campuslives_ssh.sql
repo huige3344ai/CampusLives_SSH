ac3ff3e835c510666cdd0da0f7e9434885ad9537
872958323@qq.com <872958323@qq.com>
2015-06-09 08:47:15 +0800

    失物招领数据库补交

--- campuslives_ssh.sql
+++ campuslives_ssh.sql
@@ -1,24 +1,42 @@
 /*
-Navicat MySQL Data Transfer
+SQLyog v10.2 
+MySQL - 5.5.15 : Database - campuslives_ssh
+*********************************************************************
+*/
 
-Source Server         : localhost_3306
-Source Server Version : 50515
-Source Host           : localhost:3309
-Source Database       : campuslives_ssh
+/*!40101 SET NAMES utf8 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50515
-File Encoding         : 65001
+/*!40101 SET SQL_MODE=''*/;
 
-Date: 2015-06-07 17:45:16
-*/
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`campuslives_ssh` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+USE `campuslives_ssh`;
 
--- ----------------------------
--- Table structure for `restaurant`
--- ----------------------------
+/*Table structure for table `lost` */
+
+DROP TABLE IF EXISTS `lost`;
+
+CREATE TABLE `lost` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lostname` varchar(40) NOT NULL,
+  `lostername` varchar(40) NOT NULL,
+  `address` varchar(80) NOT NULL,
+  `tel` varchar(80) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `promulgator` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `lost` */
+
+/*Table structure for table `restaurant` */
+
 DROP TABLE IF EXISTS `restaurant`;
+
 CREATE TABLE `restaurant` (
   `Num` int(11) NOT NULL AUTO_INCREMENT,
   `Rname` varchar(50) NOT NULL,
@@ -27,39 +45,28 @@ CREATE TABLE `restaurant` (
   PRIMARY KEY (`Num`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=gbk;
 
--- ----------------------------
--- Records of restaurant
--- ----------------------------
-INSERT INTO `restaurant` VALUES ('1', 'The Factory工厂餐吧', 'images/food/image/1.jpg', '4.5');
-INSERT INTO `restaurant` VALUES ('2', '肯德基', 'images/food/image/2.jpg', '5');
-INSERT INTO `restaurant` VALUES ('3', '车仔面', 'images/food/image/3.jpg', '4');
-INSERT INTO `restaurant` VALUES ('4', '猪扒包', 'images/food/image/4.jpg', '4');
-INSERT INTO `restaurant` VALUES ('5', '二饭', 'images/food/image/5.jpg', '4.5');
-INSERT INTO `restaurant` VALUES ('6', '京苑', 'images/food/image/6.jpg', '5');
-INSERT INTO `restaurant` VALUES ('7', '豪嘉基', 'images/food/image/7.jpg', '5');
-INSERT INTO `restaurant` VALUES ('8', '一饭', 'images/food/image/6.jpg', '3');
+/*Data for the table `restaurant` */
 
--- ----------------------------
--- Table structure for `role`
--- ----------------------------
+insert  into `restaurant`(`Num`,`Rname`,`images`,`price`) values (1,'The Factory工厂餐吧','images/food/image/1.jpg',4.5),(2,'肯德基','images/food/image/2.jpg',5),(3,'车仔面','images/food/image/3.jpg',4),(4,'猪扒包','images/food/image/4.jpg',4),(5,'二饭','images/food/image/5.jpg',4.5),(6,'京苑','images/food/image/6.jpg',5),(7,'豪嘉基','images/food/image/7.jpg',5),(8,'一饭','images/food/image/6.jpg',3);
+
+/*Table structure for table `role` */
+
 DROP TABLE IF EXISTS `role`;
+
 CREATE TABLE `role` (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
   `rname` varchar(20) NOT NULL,
   PRIMARY KEY (`rid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of role
--- ----------------------------
-INSERT INTO `role` VALUES ('1', '普通用户');
-INSERT INTO `role` VALUES ('2', '管理员');
-INSERT INTO `role` VALUES ('3', '商家');
+/*Data for the table `role` */
 
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
+insert  into `role`(`rid`,`rname`) values (1,'普通用户'),(2,'管理员'),(3,'商家');
+
+/*Table structure for table `user` */
+
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
   `rid` int(11) DEFAULT '1',
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -74,13 +81,11 @@ CREATE TABLE `user` (
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`rid`) REFERENCES `role` (`rid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', '3', 'huigse334', '123456', '1', '2015-05-20', '26', '659174@qq.com');
-INSERT INTO `user` VALUES ('1', '4', 'huige334', '6981658', '1', '2015-05-20', '23', '659174@qq.com');
-INSERT INTO `user` VALUES ('1', '5', 'huigse334', '123456', '1', '2015-05-20', '26', '659174@qq.com');
-INSERT INTO `user` VALUES ('1', '6', 'huige23', '6981658', '1', '2015-05-08', '14', '659174@qq.com');
-INSERT INTO `user` VALUES ('1', '7', 'huige234', '6981658', '1', '2015-05-12', '23', '654@qq.com');
-INSERT INTO `user` VALUES ('1', '9', 'huige432', '6981658', '1', '2015-05-18', '12', '6981658@qq.com');
-INSERT INTO `user` VALUES ('1', '10', 'huige789', '123456', '1', '1997-02-18', '26', '123456@163.com');
+/*Data for the table `user` */
+
+insert  into `user`(`rid`,`id`,`userName`,`password`,`sex`,`birthday`,`age`,`email`) values (1,3,'huigse334','123456','1','2015-05-20',26,'659174@qq.com'),(1,4,'huige334','6981658','1','2015-05-20',23,'659174@qq.com'),(1,5,'huigse334','123456','1','2015-05-20',26,'659174@qq.com'),(1,6,'huige23','6981658','1','2015-05-08',14,'659174@qq.com'),(1,7,'huige234','6981658','1','2015-05-12',23,'654@qq.com'),(1,9,'huige432','6981658','1','2015-05-18',12,'6981658@qq.com'),(1,10,'huige789','123456','1','1997-02-18',26,'123456@163.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
